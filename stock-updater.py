93782cce3fc2b89fcd901710b678bfe6872a9b36
Mikhail Ermakov <melhiour@gmail.com>
2024-12-31 22:54:19 +0000

    Update stock-updater.py

--- stock-updater.py
+++ stock-updater.py
@@ -46,6 +46,7 @@ def get_stock_price_and_update_lunchmoney(
 
 
 if __name__ == "__main__":
+    print(sys.argv)
     if len(sys.argv) > 1:
         POLIGON_API = sys.argv[1]
         LUNCHMONEY_API = sys.argv[2]
