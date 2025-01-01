c70474064e2fd7a96c2618f975329a9324c39ee5
Mikhail Ermakov <melhiour@gmail.com>
2025-01-01 12:22:29 +0000

    Update stock-updater.py

--- stock-updater.py
+++ stock-updater.py
@@ -5,25 +5,22 @@ import requests
 POLYGON_ENDPOINT = "https://api.polygon.io/v2/aggs/ticker/"
 LUNCHMONEY_ENDPOINT = "https://dev.lunchmoney.app/v1/assets/"
 
+
 def parse_yaml(filename):
-    print("Parsing YAML")
     with open(filename) as file:
         result = yaml.load(file, Loader=yaml.FullLoader)
     return result
 
 
 def get_poligon_endpoint(stock_code, apikey):
-    print("get_poligon_endpoint")
     return POLYGON_ENDPOINT + stock_code + "/prev?adjusted=true&apiKey=" + apikey
 
 
 def get_lunchmoney_endpoint(asset_id):
-    print("get_lunchmoney_endpoint")
     return LUNCHMONEY_ENDPOINT + str(asset_id)
 
 
 def get_stock_price(stock_code, apikey):
-    print("get_stock_price")
     endpoint = get_poligon_endpoint(stock_code, apikey)
     previos_close_price = requests.get(endpoint).json()["results"][0]["c"]
     return previos_close_price
@@ -37,12 +34,10 @@ def get_usd_to_eur_price(apikey):
 
 
 def calculate_value(stock_price, amount, ust_to_eur):
-    print("calculate_value")
     return stock_price * amount * ust_to_eur
 
 
 def update_lunchmoney(asset_id, apikey, amount):
-    print("update_lunchmoney")
     data = {"balance": amount}
     headers = {"Authorization": "Bearer "+apikey}
     endpoint = get_lunchmoney_endpoint(asset_id)
@@ -52,7 +47,6 @@ def update_lunchmoney(asset_id, apikey, amount):
 def get_stock_price_and_update_lunchmoney(
     stock_code, asset_id, volume, poligon_apikey, lunchmoney_apikey
 ):
-    print("get_stock_price_and_update_lunchmoney")
     stock_price = get_stock_price(stock_code, poligon_apikey)
     usd_to_eur = get_usd_to_eur_price(poligon_apikey)
     value = calculate_value(stock_price, volume, usd_to_eur)
